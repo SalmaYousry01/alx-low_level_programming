563c01d2964bed890da3a094f362590fbe95c41e
Salma <syamh26701@gmail.com>
2023-08-10 16:58:37 -0700

    4-puts.c added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,13 @@
 /**
  * main - prints "Programming is like building a multilingual"
  * puzzle, followed by a newline."
- * return 0.
+ * Return: Always 0.
  */
 
 int main(void)
+
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	return (0);
 }
