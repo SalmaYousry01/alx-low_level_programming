12480f36dc858946fd08a9bacb65ac517f4be007
Salma <syamh26701@gmail.com>
2023-08-12 14:38:16 -0700

    4-print_alphabt.c added

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,13 @@ int main(void)
 {
 	int n = 97;
 
-	while (n <= 122 && n != 101 && n != 113)
+	while (n <= 122)
 	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
 		putchar(n);
 		n++;
 	}
