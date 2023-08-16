81fa2bf4399e50a467be64197515c22e624d8600
Salma <syamh26701@gmail.com>
2023-08-16 13:18:51 -0700

    0-putchar.c added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: c functions
+ *Return: return 0
+ */
+void putcharFunction(void);
+int main(void)
+{
+	putcharFunction();
+	return (0);
+}
+void putcharFunction(void)
+{
+	printf("_putchar\n");
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+it's all about c
